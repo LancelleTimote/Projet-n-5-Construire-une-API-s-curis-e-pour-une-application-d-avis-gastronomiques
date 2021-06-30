092cca6b7c20bf7d49b57e1191778353938277d9
Timoté LANCELLE <timote.lancelle@outlook.fr>
2021-06-30 14:05:12 +0200

    Correction des 2 routes GET, ajout routes UPDATE et DELETE

--- backend/app.js
+++ backend/app.js
@@ -31,16 +31,31 @@ app.post('/api/sauces', (req, res, next) => {    //post pour traiter seulement l
     .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/sauces/:id', (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })  //pour modifier un thing dans la base de donnée, le premier argument c'est l'objet de comparaison pour
+  //savoir quel objet on modifie (celui dont l'id est égal à l'id qui est envoyé dans les paramètres de requête), le deuxième argument c'est la nouvelle version de l'objet (spread
+  //operator pour récupérer le thing dans la requête, on ajoute que l'id correspond à celui des paramètres)
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id }) //pour supprimer, qui prend l'objet de comparaison comme argument, comme updateOne
+                                          //dans la bdd
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.get('/api/sauces/:id', (req, res, next) => { //premier segment dynamique, le frontend va envoyer l'id de l'objet, pour pouvoir aller chercher cette id on utilise ":id"
   Sauce.findOne({ _id: req.params.id }) //on utilise le modéle mongoose avec la méthode findOne pour trouver un seul objet, et on passe un objet de comparaison, là on veut que l'id
                                         //du Thing soit le même que le paramètre de requête au dessus
-    .then(thing => res.status(200).json(thing)) //on retrouve le thing s'il existe dans la BDD, et on le renvoie en réponse au frontend
+    .then(sauce => res.status(200).json(sauce)) //on retrouve le thing s'il existe dans la BDD, et on le renvoie en réponse au frontend
     .catch(error => res.status(404).json({ error })); //404 pour objet non trouvé
 });
 
 app.get('/api/sauces', (req, res, next) => { //le premier argument est l'URL visé par l'application (le endpoint, la route), l'url total serait http://localhost:3000/api/stuff
   Sauce.find()  //on utilise le modéle mongoose avec la méthode find pour avoir la liste des objets, elle retourne une promise
-  .then(things => res.status(200).json(things)) //on récupère le tableau de tous les things retourné par la BDD, on les renvoie en réponse au frontend, 200 code ok
+  .then(sauces => res.status(200).json(sauces)) //on récupère le tableau de tous les things retourné par la BDD, on les renvoie en réponse au frontend, 200 code ok
   .catch(error => res.status(400).json({ error }));
 });
 
